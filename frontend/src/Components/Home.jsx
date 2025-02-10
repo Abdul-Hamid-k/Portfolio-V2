a324f74e9953d0a1ce699fb2474276722951cbf0
Abdul Hamid Khatri <abdulhamidk0172@gmail.com>
2025-02-10 13:12:17 +0530

    feature(Home): HeroImage Visual corrections

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -7,16 +7,16 @@ const Home = () => {
   return (
     <div id='home' className='max-w-[65rem] mx-auto py-[3rem] md:py-[8rem] min-h-screen px-5 grid grid-cols-12 grid-rows-2 md:grid-rows-1 gap-2 md:gap-5 items-center '>
       <div className="flex flex-col gap-4 row-span-1">
-        <i className="ri-instagram-line text-xl"></i>
-        <i className="ri-linkedin-box-fill text-xl"></i>
-        <i className="ri-github-fill text-xl"></i>
+        <a href="" target='_blank'><i className="cursor-pointer ri-instagram-line text-xl"></i></a>
+        <a href="https://www.linkedin.com/in/abdul-hamid-khatri/" target='_blank'><i className="cursor-pointer ri-linkedin-box-fill text-xl"></i></a>
+        <a href="https://github.com/Abdul-Hamid-k" target='_blank'><i className="cursor-pointer ri-github-fill text-xl"></i></a>
       </div>
 
       <div className="order-2 md:order-0 col-start-2 col-span-11 md:col-span-6">
-        <h2 className='text-2xl font-medium'>Abdul Hamid 👋</h2>
+        <h2 className='text-2xl md:text-4xl mb-2 font-medium'>Abdul Hamid 👋</h2>
         <div className="flex">
-          <div className="h-[0.125rem] mt-3 mr-2 bg-gray-dd dark:bg-white w-20"></div>
-          <h3> Frontend Developer (React) and UI/UX Designer</h3>
+          <div className="h-[0.125rem] mt-3 mr-2 bg-gray-dd dark:bg-white w-20 "></div>
+          <h3 className='text-lg'> Frontend Developer (React) and UI/UX Designer</h3>
 
 
         </div>
