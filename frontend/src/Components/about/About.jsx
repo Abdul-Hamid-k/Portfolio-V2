9f2cb908314ba92ec30fdb0cecafd48f1f925371
Abdul Hamid Khatri <abdulhamidk0172@gmail.com>
2025-02-12 12:53:06 +0530

    frontend(about): visual corrections

--- frontend/src/Components/about/About.jsx
+++ frontend/src/Components/about/About.jsx
@@ -21,11 +21,11 @@ const About = () => {
         {/* details */}
         <div className="p-3 md:p-5 flex flex-col gap-9 items-center lg:items-start">
           {/* badges */}
-          <div className="grid grid-cols-3 w-full justify-items-center gap-2 text-l-primary dark:text-d-secondary">
+          <div className="grid grid-cols-3 w-full justify-items-center gap-2 text-l-primary dark:text-d-primary">
             {/* Experience badge */}
             <div className="border-[0.015rem] w-full rounded-md px-3 py-4  md:py-4 md:px-5 lg:px-6 border-l-secondary dark:border-d-secondary flex flex-col items-center gap-1">
               <i className="ri-award-line text-2xl"></i>
-              <h4 className="font-medium text-sm md:text-base">Experience</h4>
+              <h4 className="font-medium text-sm md:text-base ">Experience</h4>
               <h5 className="text-xs text-l-secondary dark:text-d-secondary">{`${user.experienceYear < 2 ? 'Fresher' : user.experienceYear > 2 ? 'Senior' : 'Lead'}`}</h5>
               <h5 className="hidden md:block text-center text-xs font-light text-l-secondary dark:text-d-secondary">{`(${user.experienceYear}.${user.experienceMonths} Years)`}</h5>
             </div>
@@ -54,7 +54,7 @@ const About = () => {
           <a
             href={CV}
             download=""
-            className='w-fit bg-l-primary dark:bg-d-primary text-d-primary dark:text-l-primary font-medium px-4 py-3 md:py-4 rounded-xl text-xl items-center'>Download CV
+            className='w-fit bg-l-primary dark:bg-d-primary text-d-primary dark:text-l-primary font-medium px-4 py-3 rounded-xl text-xl items-center'>Download CV
             <div className="inline-block ml-2">
               <i className="ri-send-plane-fill text-2xl"></i>
             </div>
