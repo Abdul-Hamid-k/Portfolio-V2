fe89edaab7aba0cd5960870fad6ab5f4b823f732
Abdul Hamid Khatri <abdulhamidk0172@gmail.com>
2025-02-14 20:25:42 +0530

    backend(admin-panel-logout): backend integrated, clear cookies, token and user

--- frontend/src/Components/admin-panel/Header.jsx
+++ frontend/src/Components/admin-panel/Header.jsx
@@ -1,11 +1,16 @@
-import React, { useRef, useState } from 'react'
-import { HashLink } from 'react-router-hash-link';
+import React, { useContext, useRef, useState } from 'react'
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import axios from 'axios';
+import { useNavigate } from 'react-router'
+import { UserDataContext } from '../../context/UserContext'
 
 const Header = (props) => {
 
   let headerRef = useRef(null)
+  const { setUser } = useContext(UserDataContext)
+
+  const navigate = useNavigate()
 
   useGSAP(() => {
     const tl = gsap.timeline()
@@ -18,17 +23,37 @@ const Header = (props) => {
     })
   })
 
+  const logoutUser = () => {
+    axios.post(import.meta.env.VITE_API_BASE_URL + '/user/logout', {})
+      .then((res) => {
+        if (res.status === 200) {
+          setUser(null);
+          localStorage.removeItem('token');
+          console.log('User logged out successfully')
+          navigate('/admin-panel-login')
+        }
+      })
+      .catch((err) => {
+        console.error("Error logging out: " + err)
+      });
+  }
+
   // console.log('props ', props)
 
   return (
     <>
       <header className='fixed w-full z-10 backdrop-blur-lg text-l-primary dark:text-d-primary dark:bg-black/50 select-none '>
-        <nav ref={elem => headerRef = elem} className='flex gap-3 items-center py-4 px-5 h-[4.5rem]  max-w-[75rem] mx-auto'>
-          {/* TODO */}
-          <div onClick={(prev) => props.setIsSidemenuOpen(!props.isSidemenuOpen)}>
-            <i className="block cursor-pointer sm:hidden ri-menu-4-line text-lg" ></i>
+        <nav ref={elem => headerRef = elem} className='flex gap-3 items-center justify-between py-4 px-5 h-[4.5rem]  max-w-[75rem] mx-auto'>
+          <div>
+            <div onClick={(prev) => props.setIsSidemenuOpen(!props.isSidemenuOpen)}>
+              <i className="block cursor-pointer sm:hidden ri-menu-4-line text-lg" ></i>
+            </div>
+            <li className='list-none text-lg font-medium'>Admin Panel</li>
           </div>
-          <li className='list-none text-lg font-medium'>Admin Panel</li>
+
+          <div onClick={logoutUser} className="bg-l-secondary dark:bg-l-secondary rounded-full h-12 w-12 flex justify-center items-center">
+            <i className="ri-logout-box-r-line text-xl cursor-pointer"></i>
+          </div>
         </nav >
 
       </header >
