dd7d48be2d4ad0d36c76002ffa2a23b8a150cfda
Abdul Hamid Khatri <abdulhamidk0172@gmail.com>
2025-02-14 08:17:22 +0530

    frontend(admin-panel): Admin panel login responsive UI complete for light and dark theme

---
+++ frontend/src/Components/admin-panel/AuthWrapper.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const AuthWrapper = (children) => {
+  return children
+
+}
+
+export default AuthWrapper
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,27 @@ import App from './App.jsx'
 import 'remixicon/fonts/remixicon.css'
 import './App.css'
 import UserContext, { UserDataContext } from './context/UserContext.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import AuthWrapper from './Components/admin-panel/AuthWrapper.jsx'
+import AdminPanel from './Components/admin-panel/AdminPanel.jsx'
+import Login from './Components/admin-panel/Login.jsx'
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />,
+}, {
+  path: '/admin-panel-login',
+  element: <Login />
+}, {
+  path: '/admin-panel',
+  element: <AuthWrapper> <AdminPanel /> </AuthWrapper>
+}
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserContext>
-      <App />
+      <RouterProvider router={router} />
     </UserContext>
   </StrictMode>,
 )
